ee2139f69c5083ff28520a055311dc309b05bac5
Svyatoslav Shypka <svyat200909@gmail.com>
2023-03-29 14:11:14 +0200

    Initial commit Task1_1 finished java -jar myname.jar - works Used Gson .gitignore formed

---
+++ src/main/java/com/goit/model/dto/NameDto.java
@@ -0,0 +1,14 @@
+package com.goit.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class CurrencyDto {
+    private String name;
+    private String lastname;
+
+}
